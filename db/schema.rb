491148ca3d7baa54cff0a61a135abcc74bda8400
blueplanet <erguolinge@gmail.com>
2017-02-18 22:09:39 +0900

    Add posts count

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170218130743) do
+ActiveRecord::Schema.define(version: 20170218130917) do
 
   create_table "posts", force: :cascade do |t|
     t.string   "title"
@@ -24,8 +24,9 @@ ActiveRecord::Schema.define(version: 20170218130743) do
 
   create_table "users", force: :cascade do |t|
     t.string   "name"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+    t.integer  "posts_count"
   end
 
 end
