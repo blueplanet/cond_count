dfad93e5c41038f45c344468897cc48390f8bd82
blueplanet <erguolinge@gmail.com>
2017-02-18 22:19:49 +0900

    count published

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,4 +2,5 @@ class Post < ApplicationRecord
   belongs_to :user
 
   enum status: [:draft, :review, :published]
+  counter_culture :user, column_name: Proc.new { |post| post.published? ? 'posts_count' : nil }
 end
